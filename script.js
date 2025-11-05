9bdb9bdcd16faf90e88fbbbd5130c74a6f93b513
isadiva123 <maria.silva.ferrari@escola.pr.gov.br>
2025-11-05 08:24:41 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
 const avanca = document.querySelectorAll('.btn-proximo');
 
-avanca.forEach(button => {
-    button.addEventListener('click', function(){
-        const atual = document.querySelector('.ativo');
-        const proximoPasso = 'passo-' + this.getAttribute('data-proximo');
+avanca.forEach(button => ) {
+button.addEventListener('click', function(){
+const atual = document.querySeletor('ativo');
+const proximoPasso = 'passo-' + this.getAttribute('data-proximo');
 
-        atual.classList.remove('ativo');
-        document.getElementById(proximoPasso).classList.add('ativo');
-    })
-
+atual.classlist.remove('ativo');
+document.getElementaryById(proximoPasso).classlist.add('ativo');
+  })
 })
